be0df0ce54220b45aaf77a69e2b8b53e6e51e74b
Michael Doyle <mdoyle@truecoders.io>
2019-11-01 15:31:21 -0500

    Update ChallengesSet05.cs

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -7,104 +7,37 @@ namespace ChallengesWithTestsMark8
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            return ((startNumber / n) + 1) * n;
+            throw new NotImplementedException();
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            for (int i = 0; i < businesses.Length; i++)
-            {
-                if (businesses[i].TotalRevenue == 0)
-                {
-                    businesses[i].Name = "CLOSED";
-                }
-            }
+            throw new NotImplementedException();
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            if (numbers == null)
-                return false;
-
-            for (int i = 1; i < numbers.Length; i++)
-            {
-                if (numbers[i] < numbers[i - 1])
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            throw new NotImplementedException();
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            int sum = 0;
-
-            for (int i = 1; i < numbers.Length; i++)
-            {
-                if (numbers[i - 1] % 2 == 0)
-                {
-                    sum += numbers[i];
-                }
-            }
-
-            return sum;
+            throw new NotImplementedException();
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            if (words == null || words.Length == 0)
-            {
-                return "";
-            }
-
-            string sentence = "";
-
-            foreach (string word in words)
-            {
-                if (word.Trim().Length > 0)
-                {
-                    sentence += word.Trim() + " ";
-                }
-            }
-
-            if (sentence.Length == 0)
-            {
-                return "";
-            }
-
-            sentence = sentence.Substring(0, sentence.Length - 1);
-            sentence += ".";
-            return sentence;
+            throw new NotImplementedException();
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
         {
-            List<double> everyFourth = new List<double>();
-
-            for (int i = 3; i < elements.Count; i += 4)
-            {
-                everyFourth.Add(elements[i]);
-            }
-
-            return everyFourth.ToArray();
+            throw new NotImplementedException();
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            for (int i = 0; i < nums.Length; i++)
-            {
-                for (int k = i + 1; k < nums.Length; k++)
-                {
-                    if (nums[i] + nums[k] == targetNumber)
-                    {
-                        return true;
-                    }
-                }
-            }
-
-            return false;
+            throw new NotImplementedException();
         }
     }
 }
