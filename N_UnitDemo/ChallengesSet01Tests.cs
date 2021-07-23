e0aea152979115b85f1df01f9358a15a33efc738
johndward01 <johndward01@gmail.com>
2021-07-23 15:03:31 -0500

    finished Set01 N-Unit tests, beginning Set02

--- N_UnitDemo/ChallengesSet01Tests.cs
+++ N_UnitDemo/ChallengesSet01Tests.cs
@@ -45,8 +45,75 @@ namespace N_UnitDemo
         [TestCase(-2.2, 1.1, -3.3)]
         public void Subtract(double minuend, double subtrahend, double expectedDifference)
         {
-            //var actual = _set01.Subtract
-            throw new NotImplementedException();
+            var actual = _set01.Subtract(minuend, subtrahend);
+            Assert.AreEqual(expectedDifference, actual);
         }
+
+        [TestCase(10, 7, 17)]
+        [TestCase(100, 75, 175)]
+        [TestCase(1, 1, 2)]
+        [TestCase(10, 15, 25)]
+        [TestCase(0, 7, 7)]
+        [TestCase(0, 0, 0)]
+        [TestCase(-5, 5, 0)]
+        [TestCase(-10, -7, -17)]
+        [TestCase(-10, -15, -25)]
+        public void Add(int addend1, int addend2, int expectedSum)
+        {
+            var actual = _set01.Add(addend1, addend2);
+            Assert.AreEqual(expectedSum, actual);
+        }
+
+        [TestCase(10, 7, 7)]
+        [TestCase(100, 75, 75)]
+        [TestCase(1, 1, 1)]
+        [TestCase(10, 15, 10)]
+        [TestCase(0, 7, 0)]
+        [TestCase(0, 0, 0)]
+        [TestCase(-5, 5, -5)]
+        [TestCase(-10, -7, -10)]
+        [TestCase(-10, -15, -15)]
+        public void GetSmallestNumber(int number1, int number2, int expected)
+        {
+            var actual = _set01.GetSmallestNumber(number1, number2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(10, 7, 70)]
+        [TestCase(100, 75, 7500)]
+        [TestCase(1, 1, 1)]
+        [TestCase(10, 15, 150)]
+        [TestCase(0, 7, 0)]
+        [TestCase(0, 0, 0)]
+        [TestCase(-5, 5, -25)]
+        [TestCase(-10, -7, 70)]
+        [TestCase(-10, -15, 150)]
+        [TestCase(1234567, 7654321, 9449772114007)]
+        [TestCase(1234567, -7654321, -9449772114007)]
+        public void Multiply(long factor1, long factor2, long product)
+        {
+            var actual = _set01.Multiply(factor1, factor2);
+            Assert.AreEqual(product, actual);
+        }
+
+        [TestCase("John", "Hello, John!")]
+        [TestCase("Leigh", "Hello, Leigh!")]
+        [TestCase("John Thomas", "Hello, John Thomas!")]
+        [TestCase("Leigh Ann", "Hello, Leigh Ann!")]
+        [TestCase("", "Hello!")]
+        // [InlineData(null, "Hello!")] // Assumption: string will not be null
+        public void GetGreeting(string personName, string expectedGreeting)
+        {
+            var actual = _set01.GetGreeting(personName);
+            Assert.AreEqual(expectedGreeting, actual);
+        }
+
+        [Test]
+        public void GetHey()
+        {
+            var actual = _set01.GetHey();
+            Assert.AreEqual("HEY!", actual);
+        }
+
     }
 }
---
+++ N_UnitDemo/ChallengesSet02Tests.cs
@@ -0,0 +1,38 @@
+﻿using ChallengesWithTestsMark8;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace N_UnitDemo
+{
+    [TestFixture]
+    public class ChallengesSet02Tests
+    {
+        private ChallengesSet02 _set02;
+
+        [SetUp]
+        public void Setup()
+        {
+            _set02 = new ChallengesSet02();
+        }
+
+        [Test]
+        public void CharacterIsALetter()
+        {
+            string lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            foreach (var letter in lowercaseAlphabet)
+            {
+                var lowercaseActual = _set02.CharacterIsALetter(letter);
+                var uppercaseActual = _set02.CharacterIsALetter(char.ToUpper(letter));
+
+                Assert.IsTrue(lowercaseActual);
+                Assert.IsTrue(uppercaseActual);
+            }
+        }
+
+
+
+    }
+}
