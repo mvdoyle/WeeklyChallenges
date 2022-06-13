dc1761c21b78832902b926a4e0898a74e290e20f
Miah Harper <harpermiah@yahoo.com>
2022-06-13 10:30:08 -0400

    finished test 1

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,7 +8,7 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
+            if (num1 == num2)
             {
                 return true;
             }
@@ -18,32 +20,73 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+
+            return minuend - subtrahend;
+
+            //throw new NotImplementedException();
+
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+
+            return number1 + number2;
+
+
+            //throw new NotImplementedException();
+
+
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+
+
+            if (number1 < number2)
+            {
+                return number1;
+            }
+
+            return number2;
+
+            //throw new NotImplementedException();
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+
+            return factor1 * factor2;
+
+            //throw new NotImplementedException();
+
+
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == " ")
+            {
+                return "Hello";
+            }
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+
+
+                //ternary
+                //return string.IsNullOrEmpty(nameOfPerson) ? "hello!" : $"Hello, {nameOfPerson}!";
+
+                //throw new NotImplementedException();
+        }   }
+
+            public string GetHey()
+            {
+                return "HEY!";
+
+                //throw new NotImplementedException();
+
+            }
         }
 
-        public string GetHey()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
